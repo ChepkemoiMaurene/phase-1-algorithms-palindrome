c46d05e3c3c04648b05dfbedd07141ec423796ef
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-08-26 22:09:22 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+function isPalindrome(word){
+    if(typeof word === 'number'){
+      return word === reverseInt(word)
+    }
+    return word === reverseString(word)
+  }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
+function reverseString(word){
+    return word.split('').reverse().join('')
+  }
+  function reverseInt(word){
+    return parseInt(word.toString().split('').reverse().join(''))
+  }
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
